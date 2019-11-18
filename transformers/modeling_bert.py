4f7d8ad05538cf24ca18252ebfa9677813432baf
Philipp Dufter <philipp.dufter@gmail.com>
2019-11-18 16:05:58 +0100

    Default message

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -240,7 +240,6 @@ class BertSelfAttention(nn.Module):
         # Add CNN
         if self.add_cnn:
             pass
-            #print("WILL ADD CNN HERE.")
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
